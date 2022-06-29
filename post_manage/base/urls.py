2e9f38fbe2d40a5c1c7d687e5a4dfd8a52388147
Peter Likavec <102680332+L3xooo@users.noreply.github.com>
2022-06-29 21:43:42 +0200

    Delete post_manage directory

--- post_manage/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.home,name = 'home'),
-    path('create',views.createView,name = 'create'),
-    path('delete/<str:pk>',views.deleteView,name = 'delete'),
-    path('edit/<str:pk>',views.editView,name = 'edit'),
-]
